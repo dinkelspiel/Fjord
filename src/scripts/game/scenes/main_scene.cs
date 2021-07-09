1092fdb7842bb1604307b4b60e734a004e3abad0
willem <60363370+willmexe@users.noreply.github.com>
2021-07-09 11:08:13 +0200

    Update main_scene.cs
    Removed unused rect in main_scene

--- src/scripts/game/scenes/main_scene.cs
+++ src/scripts/game/scenes/main_scene.cs
@@ -5,9 +5,6 @@ using SDL2;
 
 namespace Proj.Game {
     public class main_scene : scene {
-        int x, y = 0;
-        int move_speed = 5;
-
         gui_element bg, sidebar, button;
         bool pressed;
 
@@ -31,18 +28,6 @@ namespace Proj.Game {
         }
 
         public override void update() {
-            if(input.get_key_pressed(input.key_w)) {
-                y -= move_speed;
-            } else if(input.get_key_pressed(input.key_s)) {
-                y += move_speed;
-            }
-
-            if(input.get_key_pressed(input.key_a)) {
-                x -= move_speed;
-            } else if(input.get_key_pressed(input.key_d)) {
-                x += move_speed;
-            }
-
             if(button.mouse_hovered()) {
                 button.set_color(200, 200, 200, 255, 3);
                 if(mouse.button_just_pressed("lmb")) {
@@ -65,12 +50,6 @@ namespace Proj.Game {
 
         public override void render() {
             bg.render();
-
-            SDL.SDL_Rect rect;
-            rect.x = x;
-            rect.y = y;
-            rect.w = rect.h = 100;
-            //draw.rect(game_manager.renderer, rect, 255, 255, 255, 255, true);
         }
     }
 }
