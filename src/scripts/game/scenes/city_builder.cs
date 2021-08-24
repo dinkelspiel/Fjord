f06d02d989283fb5a45923bb1644494b646c4f79
willem <60363370+willmexe@users.noreply.github.com>
2021-08-24 18:31:59 +0200

    Cleaned up scene loader section.

---
+++ src/scripts/game/scenes/city_builder.cs
@@ -0,0 +1,29 @@
+using Proj.Modules.Input;
+using Proj.Modules.Ui;
+using Proj.Modules.Debug;
+using Proj.Modules.Graphics;
+using SDL2;
+using System;
+using System.Numerics;
+
+namespace Proj.Game {
+    public class city_builder : scene {
+
+        public city_builder() {
+            
+        }
+
+        public override void on_load()
+        {
+            game_manager.set_render_resolution(game_manager.renderer, 300, 169);
+        }
+
+        public override void update() {
+
+        }
+
+        public override void render() {
+
+        }
+    }
+}
