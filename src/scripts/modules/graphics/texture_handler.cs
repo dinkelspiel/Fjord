7266180c3ea47d6d84bfdbda826e63be469277fa
willem <60363370+willmexe@users.noreply.github.com>
2021-08-24 12:44:17 +0200

    Moved from 'Ui' to 'Graphics' because it describes it better.

---
+++ src/scripts/modules/graphics/texture_handler.cs
@@ -0,0 +1,13 @@
+using SDL2;
+using System;
+
+namespace Proj.Modules.Graphics {
+    public static class texture_handler {
+        public static IntPtr load_texture(string file, IntPtr renderer) {
+            IntPtr tmp_surface = SDL_image.IMG_Load("src/resources/" + game_manager.asset_pack + "/assets/images/" + file);
+            IntPtr texture = SDL.SDL_CreateTextureFromSurface(game_manager.renderer, tmp_surface);
+            SDL.SDL_FreeSurface(tmp_surface);
+            return texture;
+        }
+    }
+}
