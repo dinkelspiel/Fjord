5073c6996302cd036e4f4c07daa26351a81eae70
WillM <60363370+WillMexe@users.noreply.github.com>
2021-08-26 19:09:49 +0200

    Moved bloons to it's own folder and started work on a map_editor.

---
+++ src/scripts/game/scenes/bloons/node_editor.cs
@@ -0,0 +1,27 @@
+using Proj.Modules.Input;
+using Proj.Modules.Ui;
+using Proj.Modules.Debug;
+using Proj.Modules.Graphics;
+using System.Collections.Generic;
+using SDL2;
+using System;
+
+namespace Proj.Game {
+    public class node_editor : scene {
+        public node_editor() {
+
+        }
+
+        public override void on_load() {
+
+        }
+
+        public override void update() {
+
+        }
+
+        public override void render() {
+           
+        }
+    }
+}
