d2211591d2355af324b9eea07e626937f2385034
willem <60363370+willmexe@users.noreply.github.com>
2021-08-29 12:06:47 +0200

    Started adding dividers.

--- src/scripts/game/scenes/city_builder.cs
+++ src/scripts/game/scenes/city_builder.cs
@@ -17,6 +17,7 @@ namespace Proj.Game {
         SDL.SDL_Color bg, o1, o2, o3;
 
         IntPtr grass_tile;
+        IntPtr grass_divider;
 
         public int[,] Map = new int[,] {
             {1, 1, 1, 1},
@@ -60,6 +61,12 @@ namespace Proj.Game {
             game_manager.set_asset_pack("city_builder");
 
             grass_tile = texture_handler.load_texture("grass.png", game_manager.renderer);
+            grass_divider = texture_handler.load_texture("grass_divider.png", game_manager.renderer);
+
+            SDL.SDL_SetTextureBlendMode(grass_tile, SDL.SDL_BlendMode.SDL_BLENDMODE_BLEND);
+            SDL.SDL_SetTextureBlendMode(grass_divider, SDL.SDL_BlendMode.SDL_BLENDMODE_BLEND);
+
+            // SDL.SDL_SetTextureAlphaMod(grass_tile, 100);
         }
 
         public override void update() {
@@ -74,15 +81,12 @@ namespace Proj.Game {
 
                         double length = math_uti.point_distance(new Vector2(mouse.x / 4.26f, mouse.y / 4.26f), new Vector2(x, y + (int)offset));
 
-                        if(length < closest_length) {
-                            closest.x = i;
-                            closest.y = j;
-                            closest_length = length;
+                        if(length < 5) {
+                            selected.x = i;
+                            selected.y = j;
                         }
                     }
                 }
-                selected.x = closest.x;
-                selected.y = closest.y;
             }
 
             if(mouse.button_just_pressed(1)) {
@@ -102,15 +106,15 @@ namespace Proj.Game {
                     int y =  50 + j * 7;
 
                     double offset = Math.Clamp(math_uti.point_distance(new Vector2(mouse.x / 4.26f, mouse.y / 4.26f), new Vector2(x, y)), 0, 15) / 2.5;
-
-                    SDL.SDL_Rect rect;
-                    rect.x = x;
-                    rect.y = y + (int)offset;
-                    rect.w = 1;
-                    rect.h = 1;
                     
+                    uint a;
+                    int b, w, h;
+
+                    SDL.SDL_QueryTexture(grass_tile, out a, out b, out w, out h);
+
                     draw.texture_ext(game_manager.renderer, grass_tile, x, y + (int)offset, 0);
-                    draw.rect(game_manager.renderer, rect, 255, 255, 255, 255, true);
+                    // draw.texture_ext(game_manager.renderer, grass_divider, x + w / 4 + 1, y + (int)offset - 3 + h / 2 + 1, 0, true);
+                    // draw.texture_ext(game_manager.renderer, grass_divider, x + w / 4 + 1, y + (int)offset - 3, 0);
                 }
             }
 
