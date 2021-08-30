fb25f7d422e1d8f15b3f859f9988af054adf0727
WillM <60363370+WillMexe@users.noreply.github.com>
2021-08-31 01:50:02 +0200

    Updated scenes.

--- src/scripts/game_manager.cs
+++ src/scripts/game_manager.cs
@@ -78,7 +78,8 @@ namespace Proj
             scene_handler.add_scene("node_editor", new node_editor());
             scene_handler.add_scene("client", new game_client());
             scene_handler.add_scene("server", new game_server());
-            scene_handler.load_scene("server");
+            scene_handler.add_scene("tilemap", new tilemap_editor());
+            scene_handler.load_scene("tilemap");
 
             font_handler.load_font("default", "Sans", 42);
         }
