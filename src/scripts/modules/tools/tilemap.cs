a93454748db472e790d283e59dbabf9cbcb55327
WillM <60363370+WillMexe@users.noreply.github.com>
2021-08-31 01:49:47 +0200

    Started work on 'tilemap' system.

---
+++ src/scripts/modules/tools/tilemap.cs
@@ -0,0 +1,18 @@
+namespace Proj.Modules.Tools {
+    public class tilemap {
+
+        public int[,] map;
+        public int grid_w, grid_h;
+        public int w, h;
+
+        public List<string> textures = new List<string>();
+
+        public tilemap(int w, int h, int grid_w, int grid_h) {
+            map = new int[w,h];
+            this.grid_w = grid_w;
+            this.grid_h = grid_h;
+            this.w = w;
+            this.h = h;
+        }
+    }
+}
