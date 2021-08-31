75273acf60922e32a522e49d681aa3b1df0ffc5c
WillM <60363370+WillMexe@users.noreply.github.com>
2021-08-31 16:48:05 +0200

    Made 'tilemap' more useable.

--- src/scripts/modules/tools/tilemap.cs
+++ src/scripts/modules/tools/tilemap.cs
@@ -1,3 +1,6 @@
+using Proj.Modules.Graphics;
+using System.Numerics;
+
 namespace Proj.Modules.Tools {
     public class tilemap {
 
@@ -8,6 +11,7 @@ namespace Proj.Modules.Tools {
         public int w, h;
 
         public List<string> textures = new List<string>();
+        List<IntPtr> textures_intptr = new List<IntPtr>();
 
         public tilemap(int w, int h, int grid_w, int grid_h) {
             map = new int[w,h];
@@ -15,6 +19,26 @@ namespace Proj.Modules.Tools {
             this.grid_h = grid_h;
             this.w = w;
             this.h = h;
+        }
+
+        public void load_textures() {
+            string ass = game_manager.asset_pack;
+            game_manager.set_asset_pack(asset_pack);
+
+            foreach(string texture in textures) {
+                textures_intptr.Add(texture_handler.load_texture(texture, game_manager.renderer));
+            }
+
+            game_manager.set_asset_pack(ass);
+        }
+
+        public void draw_tilemap(int x, int y) {
+            for(var i = 0; i < w; i++) {
+                for(var j = 0; j < h; j++) {
+                    if(map[i, j] != -1 && map[i, j] < textures_intptr.Count)
+                        draw.texture(game_manager.renderer, textures_intptr[map[i, j]], x + i * grid_w, y + j * grid_h, 0, new Vector2(0, 0), true);
+                }
+            }
         }
     }
 }
