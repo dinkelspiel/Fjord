db381442608c703aa1235eff9551a0fd3251a8ba
willem <60363370+willmexe@users.noreply.github.com>
2021-09-01 15:56:36 +0200

    Added default texture.

--- src/scripts/modules/graphics/texture_handler.cs
+++ src/scripts/modules/graphics/texture_handler.cs
@@ -3,6 +3,14 @@ using System;
 
 namespace Proj.Modules.Graphics {
     public static class texture_handler {
+        public static IntPtr default_texture;
+
+        public static void init() {
+            IntPtr tmp_surface = SDL_image.IMG_Load("src/scripts/modules/graphics/error.png");
+            default_texture = SDL.SDL_CreateTextureFromSurface(game_manager.renderer, tmp_surface);
+            SDL.SDL_FreeSurface(tmp_surface);       
+        }
+
         public static IntPtr load_texture(string file, IntPtr renderer) {
             IntPtr tmp_surface = SDL_image.IMG_Load("src/resources/" + game_manager.asset_pack + "/assets/images/" + file);
             IntPtr texture = SDL.SDL_CreateTextureFromSurface(game_manager.renderer, tmp_surface);
