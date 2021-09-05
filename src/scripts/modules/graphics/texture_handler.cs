8c867dd41959aafc636d8882a66ff0e22152bc19
WillM <60363370+WillMexe@users.noreply.github.com>
2021-09-05 17:04:22 +0200

    Made 'load_texture' fallback to 'default_texture' instead of crashing as it did previously.

--- src/scripts/modules/graphics/texture_handler.cs
+++ src/scripts/modules/graphics/texture_handler.cs
@@ -12,8 +12,14 @@ namespace Proj.Modules.Graphics {
         }
 
         public static IntPtr load_texture(string file, IntPtr renderer) {
-            IntPtr tmp_surface = SDL_image.IMG_Load("src/resources/" + game_manager.asset_pack + "/assets/images/" + file);
-            IntPtr texture = SDL.SDL_CreateTextureFromSurface(game_manager.renderer, tmp_surface);
+            IntPtr tmp_surface = SDL_image.IMG_Load("src/resources/" + game_manager.asset_pack + "/assets/images/" + file);;
+            IntPtr texture;
+            if(File.Exists("src/resources/" + game_manager.asset_pack + "/assets/images/" + file)) {
+                texture = SDL.SDL_CreateTextureFromSurface(game_manager.renderer, tmp_surface);
+            } else {
+                texture = default_texture;
+            }
+             
             SDL.SDL_FreeSurface(tmp_surface);
             return texture;
         }
