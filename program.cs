f038df07e99808c38ddf09b76d236b090d0b9e8a
WillM <60363370+WillMexe@users.noreply.github.com>
2021-09-05 17:16:59 +0200

    Changed resolution from '720p' to '1080p'.

--- program.cs
+++ program.cs
@@ -10,8 +10,10 @@ namespace Proj
 {
     class Program
     {
+        static string[] sys_args;
+
         private static void game_thread() {
-            game_manager.init("Engine", SDL.SDL_WINDOWPOS_CENTERED, SDL.SDL_WINDOWPOS_CENTERED, 1280, 720, false);
+            game_manager.init("Engine", SDL.SDL_WINDOWPOS_CENTERED, SDL.SDL_WINDOWPOS_CENTERED, 1920, 1080, false, sys_args);
             while(game_manager.running()) {
                 event_handler.handle_events();
                 game_manager.update();
@@ -26,6 +28,7 @@ namespace Proj
         static void Main(string[] args) {
             // Task game_task = Task.Factory.StartNew(() => game_thread());
             // Task.WaitAll(game_task, debug_web_task);
+            sys_args = args;
             game_thread();
         }
     }
