37942fb64e816d4be6be5c12c54276f89eaa63c7
WillM <60363370+WillMexe@users.noreply.github.com>
2021-09-10 14:42:56 +0200

    Added hashing function.

---
+++ src/scripts/modules/misc/hash.cs
@@ -0,0 +1,29 @@
+using System.Security.Cryptography;
+
+public static class hash {
+    public static string HashString(string text)
+    {
+        var salt = "";
+        
+        if (String.IsNullOrEmpty(text))
+        {
+            return String.Empty;
+        }
+        
+        // Uses SHA256 to create the hash
+        var sha = SHA256.Create();
+        using (sha)
+        {
+            // Convert the string to a byte array first, to be processed
+            byte[] textBytes = System.Text.Encoding.UTF8.GetBytes(text + salt);
+            byte[] hashBytes = sha.ComputeHash(textBytes);
+            
+            // Convert back to a string, removing the '-' that BitConverter adds
+            string hash = BitConverter
+                .ToString(hashBytes)
+                .Replace("-", String.Empty);
+
+            return hash;
+        }
+    }
+}
