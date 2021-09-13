5612e7e70425fcb0a59224bac978338663c5f760
WillM <60363370+WillMexe@users.noreply.github.com>
2021-09-14 00:34:53 +0200

    More work on bindings.

--- sdl/SDL2_gfx.cs
+++ sdl/SDL2_gfx.cs
@@ -8,17 +8,25 @@ namespace SDL2
     public static class SDL_gfx {
         private const string nativeLibName = "SDL2_gfx";
 
+        internal static class Import
+        {
+            public const string lib = "SDL2_gfx.dll";
+        }
+
         #region SDL2_gfxPrimitives.h 
 
-		[DllImport(nativeLibName, CallingConvention = CallingConvention.Cdecl)]
-		public static extern int thickLineColor(
+		[DllImport(Import.lib, CallingConvention = CallingConvention.Cdecl)]
+		internal static extern int thickLineRGBA(
             IntPtr	renderer,
             int 	x1,
             int 	y1,
             int 	x2,
             int 	y2,
             uint 	width,
-            uint 	color 
+            uint 	r,
+            uint 	g,
+            uint 	b,
+            uint 	a 
 		);
 
         #endregion
