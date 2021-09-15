78dac4875f96045e971e23879ad3d2c6833e7fc3
WillM <60363370+WillMexe@users.noreply.github.com>
2021-09-15 10:56:26 +0200

    Added static 'data' class to load custom data files.

---
+++ src/scripts/modules/misc/data.cs
@@ -0,0 +1,10 @@
+using System.IO;
+
+namespace Fjord.Modules.Misc {
+    public static class data {
+        public static string load_data_file(string path) {
+            string file_path = game_manager.executable_path + "\\src\\resources\\" + game_manager.asset_pack + "\\data\\" + path;
+            return File.ReadAllText(file_path);
+        }
+    }
+}
