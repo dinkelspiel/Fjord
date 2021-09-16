bbc7a4ef492a7bd699611b6f3c8a0762edcb632c
willem <60363370+willmexe@users.noreply.github.com>
2021-09-16 10:23:00 +0200

    Removed 'engine_startup' scene.

--- src/scripts/modules/ui/scene_handler.cs
+++ src/scripts/modules/ui/scene_handler.cs
@@ -12,27 +12,21 @@ namespace Fjord.Modules.Ui {
     public static class scene_handler {
         private static Dictionary<string, scene> scenes = new Dictionary<string, scene>();
         private static string current_scene; 
-        public static string string_start_scene;
-        public static bool start_scene_running = false;
+        private static int scenes_loaded = 0;
 
         public static void add_scene(string id, scene scene_add) {
             scenes.Add(id, scene_add);
         }
         
         public static void load_scene(string id) {
-            if(!start_scene_running) {
-                if(current_scene != null)
-                    scenes[current_scene].on_unload();
-                current_scene = id;
-                scenes[current_scene].on_load();
-                Debug.Debug.send("Loaded scene '" + id + "' successfully!");
-            } else {
-                Debug.Debug.send("Can't load scene during game startup!");
-            }
-        }
+            if(current_scene != null)
+                scenes[current_scene].on_unload();
 
-        public static void start_scene(string id) {
-            string_start_scene = id;
+            current_scene = id;
+
+            Debug.Debug.send("Loaded scene '" + id + "' successfully!");
+
+            scenes_loaded++;
         }
 
         public static void stop() {
