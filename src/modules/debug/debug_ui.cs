5ea82e545437d4bfee3f3ea686024393683dc692
WillM <60363370+WillMexe@users.noreply.github.com>
2021-10-10 17:21:18 +0200

    Made 'debug_ui' set resolution to 1080p when drawing ui to make scaling consistent.

--- src/modules/debug/debug_ui.cs
+++ src/modules/debug/debug_ui.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Diagnostics;
+using System.Numerics;
 using Fjord.Modules.Graphics;
 
 namespace Fjord.Modules.Debug {
     public static class debug_gui
     {
         public static void draw_fps() {
+            Vector2 tmp_res = game_manager.resolution;
+            game_manager.set_render_resolution(game_manager.renderer, 1920, 1080);
             draw.text(game_manager.renderer, 10, 10, "default", 42, "FPS: " + game_manager.get_fps().ToString());
+            game_manager.set_render_resolution(game_manager.renderer, (int)tmp_res.X, (int)tmp_res.Y);
         }
     }
 }
