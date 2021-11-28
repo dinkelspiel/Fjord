cefb88b22871d3100a03b0a09bedea37cdf90b4a
WillM <60363370+WillMexe@users.noreply.github.com>
2021-11-28 09:24:41 +0100

    Added overload to 'add_component' in 'entity' to make 'parent' parameter unneeded as it is in almost all circumstaces

--- src/modules/game/entity.cs
+++ src/modules/game/entity.cs
@@ -31,6 +31,13 @@ namespace Fjord.Modules.Game
             return this;
         }
 
+        public entity add_component(component Comp) {
+            Comp.parent = this;
+            Comp.on_load();
+            components.Add(Comp);
+            return this;
+        }
+
         public entity remove_component(component Comp) {
             components.Remove(Comp);
             return this;
