c7a677447b5c9ff498449e35f0280e418b93dbdf
willmexe <imdark200@gmail.com>
2022-04-16 02:06:41 +0200

    added 'point_inside' to 'helpers'

--- src/modules/math/helpers.cs
+++ src/modules/math/helpers.cs
@@ -10,6 +10,13 @@ namespace Fjord.Modules.Mathf {
                 return false;
         }
 
+        public static bool point_inside(V2 pos, V4 rect, int margin=0) {
+            if ((pos.x > rect.x - margin && pos.x < rect.x + rect.z + margin) && (pos.y > rect.y - margin && pos.y < rect.y + rect.w + margin))
+                return true;
+            else
+                return false;
+        }
+
         public static SDL_Rect v4_to_sdl(V4 rect) {
             return new SDL_Rect(rect.x, rect.y, rect.z, rect.w);
         }
