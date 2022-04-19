6fa1328c6bafbf5a15d0831bfe75d86bb3c589dd
willmexe <imdark200@gmail.com>
2022-04-19 16:56:49 +0200

    Updated tilemap names to not reference Glacier.

--- src/modules/tilemaps/format.cs
+++ src/modules/tilemaps/format.cs
@@ -4,33 +4,33 @@ using Fjord.Modules.Graphics;
 
 namespace Fjord.Modules.Tilemaps
 {
-    public class glacier_format
+    public class tilemap
     {
-        public List<glacier_property>? properties;
+        public List<property>? properties;
         public V2? grid_size;
         public V2? tile_size;
-        public Dictionary<string, glacier_tile>? tiles;
+        public Dictionary<string, tile>? tiles;
         public List<List<Dictionary<string, dynamic>>>? tile_map;
     }
 
-    public struct glacier_property {
+    public struct property {
         public string name;
         public string datatype;
         public dynamic default_value;
 
-        public glacier_property(string name, string data, dynamic default_value) {
+        public property(string name, string data, dynamic default_value) {
             this.name = name;
             this.datatype = data;
             this.default_value = default_value;
         }
     }
 
-    public struct glacier_tile {
+    public struct tile {
         public string name;
         public string path;
         public texture tex;
 
-        public glacier_tile(string name, string path) {
+        public tile(string name, string path) {
             this.name = name;
             this.path = path;
             this.tex = new texture(path);
