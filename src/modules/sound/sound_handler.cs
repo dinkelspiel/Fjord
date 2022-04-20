eb96abbd202cf8a6242af4d724b2791213cfcf65
willmexe <imdark200@gmail.com>
2022-04-20 10:56:31 +0200

    Added more error checks.

--- src/modules/sound/sound_handler.cs
+++ src/modules/sound/sound_handler.cs
@@ -7,7 +7,10 @@ namespace Fjord.Modules.Sound {
         public static Dictionary<string, IntPtr> sounds = new Dictionary<string, IntPtr>();
 
         public static void load_sound(string path, string id) {
-            sounds.Add(id, Mix_LoadWAV( game.get_resource_folder() + "/" + game.asset_pack + "/assets/sounds/" + path + ".wav" ));
+            if(!sounds.ContainsKey(id))
+                sounds.Add(id, Mix_LoadWAV( game.get_resource_folder() + "/" + game.asset_pack + "/assets/sounds/" + path + ".wav" ));
+            else
+                Debug.Debug.send("Sounds already contains id '" + id + "'!");
         }
 
         public static void play_sound(string id) {
