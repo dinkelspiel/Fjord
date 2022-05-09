6cf126ed26eaae7627d5ef1b8724ac82db4360a9
willmexe <imdark200@gmail.com>
2022-05-09 09:40:09 +0200

    Removed bloat in 'sound' module.

--- src/modules/sound/sound_handler.cs
+++ src/modules/sound/sound_handler.cs
@@ -6,18 +6,18 @@ namespace Fjord.Modules.Sound {
     public static class Sound {
         public static Dictionary<string, IntPtr> sounds = new Dictionary<string, IntPtr>();
 
-        public static void load_sound(string path, string id) {
+        public static void load(string path, string id) {
             if(!sounds.ContainsKey(id))
                 sounds.Add(id, Mix_LoadWAV( game.get_resource_folder() + "/" + game.asset_pack + "/assets/sounds/" + path + ".wav" ));
             else
                 Debug.Debug.send("Sounds already contains id '" + id + "'!");
         }
 
-        public static void play_sound(string id) {
+        public static void play(string id) {
             Mix_PlayChannel( -1, sounds[id], 0 );
         }
 
-        public static IntPtr get_sound(string id) {
+        public static IntPtr get(string id) {
             return sounds[id];
         }
     }
