f786cfb6c23f94d161b9c1cc48b8ee4255531f10
shykeiichi <mail@keii.dev>
2023-06-20 01:27:53 +0200

    Simple camera shake implementation

--- src/Scenes/Camera.cs
+++ src/Scenes/Camera.cs
@@ -5,8 +5,12 @@ namespace Fjord.Scenes;
 public class SceneCamera
 {
     public Entity? Target;
+    private Vector2 CoreOffset = new();
     public Vector2 Offset = new();
     public float Lag = 1;
+    public float CameraShakeIntensity = 1f;
+    public float CameraShakeLife = 0f;
+    public float CameraShakeLifeMax = 0f;
     private Scene parentScene;
 
     public SceneCamera(Scene parent)
@@ -24,11 +28,34 @@ public class SceneCamera
         this.Lag = lag;
     } 
 
+    public void SetCameraShake(float life, float intensity)
+    {
+        this.CameraShakeLife = life;
+        this.CameraShakeLifeMax = life;
+        this.CameraShakeIntensity = intensity;
+    }
+
     public void Update(Vector2 WindowSize)
     {
         if(Target != null)
         {
-            Offset += ((((Target.Get<Transform>().Position - WindowSize / 2) - Offset) / Lag) * 500) * parentScene.DeltaTime;
+            CoreOffset += ((((Target.Get<Transform>().Position - WindowSize / 2) - CoreOffset) / Lag) * 500) * parentScene.DeltaTime;
+        }
+
+        if(CameraShakeLife > 0f)
+        {
+            Random random = new();
+            Offset = Vector2.Add(
+                CoreOffset, 
+                new(
+                    ((float)(random.NextDouble() - 0.5) * CameraShakeIntensity) * (CameraShakeLife / CameraShakeLifeMax),
+                    ((float)(random.NextDouble() - 0.5) * CameraShakeIntensity) * (CameraShakeLife / CameraShakeLifeMax)
+                )
+            );
+            CameraShakeLife -= 100 * Game.GetDeltaTime();
+        } else
+        {
+            Offset = CoreOffset;
         }
     }
 }
